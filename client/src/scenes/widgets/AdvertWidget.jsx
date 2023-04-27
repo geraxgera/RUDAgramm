9cc21677b73bd552c8c2d11f1c85726e0a7b3013
German Konradi <geraxgera@Germans-MBP.fritz.box>
2023-04-27 13:54:29 +0200

    first commit

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,39 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Gesponsert
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/info4.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>JaneCosmetics</Typography>
+        <Typography color={medium}>janecosmetics.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Durch diesen Weg können Sie atemberaubende und makellose Schönheit
+        erlangen, während Ihre Haut gepflegt wird. Es bewirkt ein Peeling, das
+        Ihre Haut zum Strahlen bringt wie das Licht.
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
