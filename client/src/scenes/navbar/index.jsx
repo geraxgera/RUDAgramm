1d0c9a5251d4a4449d657d7b04a1e1cd67958c02
German Konradi <geraxgera@Germans-MBP.fritz.box>
2023-08-11 14:25:50 +0200

    add new mongodburl

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+            RUDAGRAMM
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
